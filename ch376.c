537a396e78013a486903f960afcfa7257e858e52
jede <jede@oric.org>
2017-01-06 00:34:06 +0100

    Removing file create

--- ch376.c
+++ ch376.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <ch376.h>
 
+
 int main()
 {
 	unsigned  char value;
 	value=ch376_check_exist(0x55);
+	printf("This tool is for tested ch376 chip ...\n");
 	printf("Detecting ch376 ...");
 	if (value==0xaa)
 		printf("OK\n");
@@ -15,14 +17,15 @@ int main()
 	}
 	
 	printf("Ch376 version : %d\n",ch376_ic_get_version()&(32+16+8+4+2+1));
-	printf("Setting USBKEY Mode ...");
+	printf("Setting USBKEY Mode ...\n");
 	ch376_set_usb_mode(CH376_ARG_SET_USB_MODE_USB_HOST);
 	value=ch376_disk_mount();
 	if (value==CH376_INT_SUCCESS)
 		printf("USBKEY Mounted\n");
 	else
-		printf("Impossible to mount USBKEY");
-	
-	
+	{	
+		printf("Impossible to mount USBKEY\n");
+		return 1;
+	}
 	return 0;
 }
--- make.bat
+++ make.bat
@@ -1,2 +1,12 @@
+SET ORICUTRON="..\..\..\oricutron\"
+
+SET ORIGIN_PATH=%CD%
+
 cl65 -ttelemon30 ch376.c
-copy ch376 C:\Users\plifp\OneDrive\oric\oricutron\usbdrive
+copy ch376 C:\Users\plifp\OneDrive\oric\oricutron\usbdrive\bin\ch376
+
+cd %ORICUTRON%
+Oricutronoffset -mt -d teledisks\stratsed.dsk
+rem Oricutron_ch376V3 -mt -d teledisks\stratsed.dsk
+cd %ORIGIN_PATH%
+
