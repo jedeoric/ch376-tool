502e2351f9db4183667564f6912a9a60a60981d3
jede <jede@oric.org>
2017-01-06 22:03:48 +0100

    placeholder added

--- make.bat
+++ make.bat
@@ -3,10 +3,16 @@ SET ORICUTRON="..\..\..\oricutron\"
 SET ORIGIN_PATH=%CD%
 
 cl65 -ttelemon30 ch376.c
+
+copy ch376 release
+
+IF "%1"=="NORUN" GOTO End
+
 copy ch376 C:\Users\plifp\OneDrive\oric\oricutron\usbdrive\bin\ch376
 
 cd %ORICUTRON%
 Oricutronoffset -mt -d teledisks\stratsed.dsk
 rem Oricutron_ch376V3 -mt -d teledisks\stratsed.dsk
 cd %ORIGIN_PATH%
+:End
 
