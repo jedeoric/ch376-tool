de9e17423970a7a8fe4a1f3ebf097daebe43b129
jede <jede@oric.org>
2017-02-18 13:22:43 +0100

    Correcting path

--- make.bat
+++ make.bat
@@ -4,11 +4,11 @@ SET ORIGIN_PATH=%CD%
 
 cl65 -ttelestrat ch376.c ..\oric-common\lib\ca65\ch376.s
 
-copy ch376 release
+copy ch376 release\usr\bin\
 
 IF "%1"=="NORUN" GOTO End
 
-copy ch376 %ORICUTRON%\usbdrive\bin\ch376
+copy ch376 %ORICUTRON%\usbdrive\usr\bin\ch376
 
 cd %ORICUTRON%
 OricutronV4 -mt -d teledisks\stratsed.dsk
